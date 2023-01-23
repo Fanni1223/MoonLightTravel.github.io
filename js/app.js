0b603a41f4a8e76f6b694368de2ec3086bebf947
Fanni1223 <juhasz.fanni-2018@keri.mako.hu>
2023-01-23 10:08:51 +0100

    Keresés

--- js/app.js
+++ js/app.js
@@ -24,31 +24,38 @@
         })
         .state('blog', {
           url: '/blog',
-          templateUrl: './html/blog.html'
+          templateUrl: './html/blog.html',
+          controller: 'appController'
         })
         .state('rolunk', {
           url: '/rolunk',
-          templateUrl: './html/rolunk.html'
+          templateUrl: './html/rolunk.html',
+          controller: 'appController'
         })
         .state('afrika', {
           url: '/afrika',
-          templateUrl: './html/afrika.html'
+          templateUrl: './html/afrika.html',
+          controller: 'appController'
         })
         .state('amerika', {
           url: '/amerika',
-          templateUrl: './html/amerika.html'
+          templateUrl: './html/amerika.html',
+          controller: 'appController'
         })
         .state('ausztralia', {
           url: '/ausztralia',
-          templateUrl: './html/ausztralia.html'
+          templateUrl: './html/ausztralia.html',
+          controller: 'appController'
         })
         .state('azsia', {
           url: '/azsia',
-          templateUrl: './html/azsia.html'
+          templateUrl: './html/azsia.html',
+          controller: 'appController'
         })
         .state('europa', {
           url: '/europa',
-          templateUrl: './html/europa.html'
+          templateUrl: './html/europa.html',
+          controller: 'appController'
         })
       
       $urlRouterProvider.otherwise('/');
